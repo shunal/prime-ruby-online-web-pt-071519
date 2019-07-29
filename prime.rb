b47f9eaaafeec142b86d7d7da8331ef45545d637
Steven Hunal <stevehunal@gmail.com>
2019-07-29 18:38:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,10 @@
 def prime?(num)
   (2..(num - 1)).each do |n|
-   
+    if num % n > 0
+      return true
+    else 
+      return false
   end
-
 end
 
 
- return false if num % n == 0
- 
-   true
